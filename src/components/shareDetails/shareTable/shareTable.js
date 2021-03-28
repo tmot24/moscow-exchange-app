3bc5cdaa86cfbb768e06755283125669b7cd9b92
Grigory Yakovlev <grigory.yakovlev96@gmail.com>
2021-03-28 22:47:46 +0300

    added table info

--- src/components/shareDetails/shareTable/shareTable.js
+++ src/components/shareDetails/shareTable/shareTable.js
@@ -23,40 +23,30 @@ export default class ShareTable extends React.Component {
 
         console.log(shareSecurities)
 
-        function createData(name, calories, fat, carbs, protein) {
-            return { name, calories, fat, carbs, protein };
+        let newRows = [];
+        for (let key in shareSecurities) {
+            const name = shareSecurities.longTitle(key);
+            const value = shareSecurities[key];
+            const obj = {name, value};
+            newRows.push(obj);
         }
-
-        const rows = [
-            createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-            createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-            createData('Eclair', 262, 16.0, 24, 6.0),
-            createData('Cupcake', 305, 3.7, 67, 4.3),
-            createData('Gingerbread', 356, 16.0, 49, 3.9),
-        ];
 
         return (
             <TableContainer component={Paper}>
                 <Table size="small" aria-label="a dense table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>Dessert (100g serving)</TableCell>
-                            <TableCell align="right">Calories</TableCell>
-                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                            <TableCell align="left" style={{fontWeight: "bold", fontSize: "medium"}}>Securities</TableCell>
+                            <TableCell align="right" style={{fontWeight: "bold", fontSize: "medium"}}>Value</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows.map((row) => (
+                        {newRows.map((row) => (
                             <TableRow key={row.name}>
                                 <TableCell component="th" scope="row">
                                     {row.name}
                                 </TableCell>
-                                <TableCell align="right">{row.calories}</TableCell>
-                                <TableCell align="right">{row.fat}</TableCell>
-                                <TableCell align="right">{row.carbs}</TableCell>
-                                <TableCell align="right">{row.protein}</TableCell>
+                                <TableCell align="right">{row.value}</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
