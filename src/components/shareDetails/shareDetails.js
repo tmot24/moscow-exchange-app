9f48e49d94d6ca77862006ea48d32740ab1ad4db
Grigory Yakovlev <grigory.yakovlev96@gmail.com>
2021-04-04 16:05:59 +0300

    refactored shareDetails.js with hooks

--- src/components/shareDetails/shareDetails.js
+++ src/components/shareDetails/shareDetails.js
@@ -1,9 +1,9 @@
 import "../app/App.css";
 import "./shareDetails.css";
 import Grid from '@material-ui/core/Grid';
-import React from "react";
+import React, {useState, useEffect} from "react";
 import WithExchangeService from "../hoc/with-exchange-service";
-import {connect} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {shareLoaded, requested, chartLoaded} from "../../actions/actions";
 import {ShareMarketData, ShareSecurities} from "../classes/currentOfShare";
 import ShareTitle from "./shareTitle/shareTitle";
@@ -13,15 +13,16 @@ import {Container} from "@material-ui/core";
 import MyChart from "./shareChart/myChart";
 import Button from '@material-ui/core/Button';
 
-class ShareDetails extends React.Component {
-    state = {
-        period: "fourMonth",
-    };
+function ShareDetails({ExchangeService, itemId}) {
+    const [period, setPeriod] = useState("fourMonth");
+    const shareArr = useSelector(state => state.shareArr);
+    const chart = useSelector(state => state.chart);
+    const dispatch = useDispatch();
 
-    currentShare () {
-        this.props.requested();
+    useEffect(() => {
+        console.log("currentShare")
+        dispatch(requested());
         // Получение объекта из API
-        const {ExchangeService, itemId} = this.props;
         ExchangeService.getCurrentShare(itemId)
             .then(obj => {
                 const shareMarketData = new ShareMarketData(...obj.marketdata.data[0]);
@@ -30,14 +31,15 @@ class ShareDetails extends React.Component {
             })
             // Запись в store
             .then(result => {
-                this.props.shareLoaded(result);
+                dispatch(shareLoaded(result));
             });
-    }
-    currentOfHistory () {
-        this.props.requested();
-        const {ExchangeService, itemId} = this.props;
+    }, [ExchangeService, dispatch, itemId])
+
+    useEffect(() => {
+        console.log("currentOfHistory")
+        dispatch(requested());
         // Получение объекта из API
-        ExchangeService.getHistoryShare(itemId, this.state.period)
+        ExchangeService.getHistoryShare(itemId, period)
             .then(obj => obj.history)
             .then(data => {
                 const dataTitle = data.columns[9];
@@ -45,80 +47,48 @@ class ShareDetails extends React.Component {
                 return [dataTitle, closePrice];
             })
             // Запись в store
-            .then(result => this.props.chartLoaded(result));
+            .then(result => dispatch(chartLoaded(result)));
+    }, [ExchangeService, dispatch, itemId, period])
+
+
+    window.scrollTo(0, 0);
+
+    if (!shareArr || !chart) {
+        return <Spinner/>;
     }
 
-    componentDidMount() {
-        this.currentShare();
-        this.currentOfHistory();
+    const shareSecurities = shareArr[1];
+    const coordinates = chart[1].map(value => {
+        return {x: (new Date(value[0])).getTime(), y: value[1]};
+    });
 
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if (this.state.period !== prevState.period) {
-            this.currentOfHistory();
-        }
-    }
-
-    updatePeriod(period) {
-        this.setState({period});
-    }
-
-    render() {
-        window.scrollTo(0, 0);
-        const {shareArr, chart} = this.props;
-
-        if (!shareArr || !chart) {
-            return <Spinner/>;
-        }
-
-        const shareSecurities = shareArr[1];
-        const coordinates = chart[1].map(value => {
-            return {x: (new Date(value[0])).getTime(), y: value[1]};
-        });
-
-        return (
-            <Container>
-                <Grid container style={{padding: 10}}>
-                    <Grid item xs={12}>
-                        <ShareTitle shareArr={shareArr}/>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <MyChart coordinates={coordinates}/>
-                        <div className={"buttons"}>
-                            <Button style={{flexBasis: 120}} variant="contained"
-                                    onClick={() => this.updatePeriod("oneWeek")}>1 неделя</Button>
-                            <Button style={{flexBasis: 120}} variant="contained"
-                                    onClick={() => this.updatePeriod("twoWeek")}>2 недели</Button>
-                            <Button style={{flexBasis: 120}} variant="contained"
-                                    onClick={() => this.updatePeriod("oneMonth")}>1 месяц</Button>
-                            <Button style={{flexBasis: 120}} variant="contained"
-                                    onClick={() => this.updatePeriod("twoMonth")}>2 месяца</Button>
-                            <Button style={{flexBasis: 120}} variant="contained"
-                                    onClick={() => this.updatePeriod("fourMonth")}>4 месяца</Button>
-                        </div>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <ShareTable shareArr={shareSecurities} title={"Дополнительная информация"}/>
-                    </Grid>
+    return (
+        <Container>
+            <Grid container style={{padding: 10}}>
+                <Grid item xs={12}>
+                    <ShareTitle shareArr={shareArr}/>
                 </Grid>
-            </Container>
-        );
-    }
+                <Grid item xs={12}>
+                    <MyChart coordinates={coordinates}/>
+                    <div className={"buttons"}>
+                        <Button style={{flexBasis: 120}} variant="contained"
+                                onClick={() => setPeriod("fourMonth")}>4 месяца</Button>
+                        <Button style={{flexBasis: 120}} variant="contained"
+                                onClick={() => setPeriod("twoMonth")}>2 месяца</Button>
+                        <Button style={{flexBasis: 120}} variant="contained"
+                                onClick={() => setPeriod("oneMonth")}>1 месяц</Button>
+                        <Button style={{flexBasis: 120}} variant="contained"
+                                onClick={() => setPeriod("twoWeek")}>2 недели</Button>
+                        <Button style={{flexBasis: 120}} variant="contained"
+                            onClick={() => setPeriod("oneWeek")}>1 неделя</Button>
+                    </div>
+                </Grid>
+                <Grid item xs={12}>
+                    <ShareTable shareArr={shareSecurities} title={"Дополнительная информация"}/>
+                </Grid>
+            </Grid>
+        </Container>
+    );
 }
 
-const mapStateToProps = (state) => {
-    return {
-        shareArr: state.shareArr,
-        chart: state.chart,
-        loading: state.loading,
-    };
-};
-
-const mapDispatchToProps = {
-    chartLoaded,
-    shareLoaded,
-    requested,
-};
-
-export default WithExchangeService()(connect(mapStateToProps, mapDispatchToProps)(ShareDetails));
+export default WithExchangeService()(ShareDetails);
