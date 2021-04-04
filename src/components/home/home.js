b8ba0493653601068453e34dbb36c495823c669a
Grigory Yakovlev <grigory.yakovlev96@gmail.com>
2021-04-04 14:07:17 +0300

    refactor with hooks home.js

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,45 +1,33 @@
 import React from "react";
-import {connect} from "react-redux";
+import {useSelector} from "react-redux";
 import MenuItem from "../menu-item/menuItem";
 import "./home.css";
 import Box from '@material-ui/core/Box';
 
-class Home extends React.Component {
+export default function Home() {
 
-    render() {
-        const {menu} = this.props;
-        const arrayObj = Object.entries(menu).map(arr => arr[1]);
+    const menu = useSelector(state => state.menu);
+    const arrayObj = Object.entries(menu).map(arr => arr[1]);
 
-        return (
-            <>
-                <div className={"greeting"}>
-                    <Box className={"greeting_box"}>
-                        <span>КУПИТЬ АКЦИИ</span>
-                        <p>ВЕДУЩИХ НЕФТЯНЫХ<br/>КОМПАНИЙ РОССИИ</p>
-                        <span>ЛЕГКО</span>
-                    </Box>
-                </div>
-                    <ul>
-                        {
-                            arrayObj.map(menuItem => {
-                                return <MenuItem
-                                    key={menuItem.id}
-                                    menuItem={menuItem}
-                                />;
-                            })
-                        }
-                    </ul>
-            </>
-
-        );
-    }
-
+    return (
+        <>
+            <div className={"greeting"}>
+                <Box className={"greeting_box"}>
+                    <span>КУПИТЬ АКЦИИ</span>
+                    <p>ВЕДУЩИХ НЕФТЯНЫХ<br/>КОМПАНИЙ РОССИИ</p>
+                    <span>ЛЕГКО</span>
+                </Box>
+            </div>
+            <ul>
+                {
+                    arrayObj.map(menuItem => {
+                        return <MenuItem
+                            key={menuItem.id}
+                            menuItem={menuItem}
+                        />;
+                    })
+                }
+            </ul>
+        </>
+    );
 }
-
-const mapStateToProps = (state) => {
-    return {
-        menu: state.menu,
-    };
-};
-
-export default connect(mapStateToProps)(Home);
