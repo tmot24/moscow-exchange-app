bedfe06012e3fcf13acb749e5d5bae9b4330cdf2
Grigory Yakovlev <grigory.yakovlev96@gmail.com>
2021-04-04 16:41:18 +0300

    refactored with useContext hook

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,29 +5,34 @@ import {Home} from "../home/home";
 import {AppHeader} from "../header/header";
 import {Route, Switch} from "react-router-dom";
 import {Basket} from "../basket/basket";
+import {ExchangeServiceContext} from "../exchange-service-context/exchange-service-context";
+import ExchangeService from "../../services/exchange-service";
+
+const exchangeService = new ExchangeService();
 
 
 const App = () => {
     return (
-        <Route>
-            <AppHeader/>
-            <div className="App">
-                <Switch>
-                    <Route path={"/"} exact component={Home}/>
-                    <Route path={"/basket"} exact component={Basket}/>
-                    <Route path={"/details/:id"} render={
-                        ({match}) => {
-                            const {id} = match.params;
-                            return <ShareDetails itemId={id}/>;
-                        }}/>
-                </Switch>
-                <img style={{position: "fixed", bottom: 10, right: 10, height: 100,}}
-                     src="https://img.pngio.com/work-in-progress-png-96-images-in-collection-page-2-work-in-progress-png-527_300.png"
-                     alt={"img"}/>
-            </div>
-        </Route>
+        <ExchangeServiceContext.Provider value={exchangeService}>
+            <Route>
+                <AppHeader/>
+                <div className="App">
+                    <Switch>
+                        <Route path={"/"} exact component={Home}/>
+                        <Route path={"/basket"} exact component={Basket}/>
+                        <Route path={"/details/:id"} render={
+                            ({match}) => {
+                                const {id} = match.params;
+                                return <ShareDetails itemId={id}/>;
+                            }}/>
+                    </Switch>
+                    <img style={{position: "fixed", bottom: 10, right: 10, height: 100,}}
+                         src="https://img.pngio.com/work-in-progress-png-96-images-in-collection-page-2-work-in-progress-png-527_300.png"
+                         alt={"img"}/>
+                </div>
+            </Route>
+        </ExchangeServiceContext.Provider>
     );
 };
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,23 +6,18 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 
 import App from './components/app/App';
 import ErrorBoundary from "./components/error/error-boundary";
-import ExchangeService from "./services/exchange-service";
-import {ExchangeServiceContext} from "./components/exchange-service-context/exchange-service-context";
 import store from "./store";
 import './index.css';
 
-const exchangeService = new ExchangeService();
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
             <CssBaseline/>
             <ErrorBoundary>
-                <ExchangeServiceContext.Provider value={exchangeService}>
-                    <Router>
-                        <App/>
-                    </Router>
-                </ExchangeServiceContext.Provider>
+                <Router>
+                    <App/>
+                </Router>
             </ErrorBoundary>
         </Provider>
     </React.StrictMode>,
