3d4bd63c196d8ac5423aa6714cf6166635525e38
Grigory Yakovlev <grigory.yakovlev96@gmail.com>
2021-04-05 22:59:53 +0300

    refactor header, added basket and home icons

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,54 @@
 import React from 'react';
-import cartIcon from './shopping-cart-solid.svg';
-import {Link} from "react-router-dom";
-import "./header.css";
+import {makeStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Badge from '@material-ui/core/Badge';
+import {withStyles} from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import {useSelector} from "react-redux";
+import HomeIcon from '@material-ui/icons/Home';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
-export function AppHeader () {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    bar: {
+        justifyContent: "space-between",
+    },
+    bgIcon: {
+        color: theme.palette.common.white,
+    },
+    icon: {
+        color: theme.palette.common.white,
+        fontSize: 35,
+    }
+}));
+const StyledBadge = withStyles((theme) => ({
+    badge: {
+        top: 7,
+        border: `2px solid ${theme.palette.background.paper}`,
+        padding: '0 4px',
+    },
+}))(Badge);
+
+export function AppHeader() {
+    const classes = useStyles();
+    const basket = useSelector(state => state.basket);
     return (
-        <div className={"head"}>
-            <div>
-                <Link to={"/"} style={{margin: 20, textDecoration: "none", fontSize: 20, fontWeight: "bold"}}>HOME</Link>
-            </div>
-            <Link to={"/basket"}>
-                <img style={{width: 30, margin: 20}} src={cartIcon} alt="basket"/>
-            </Link>
+        <div className={classes.root}>
+            <AppBar position="fixed">
+                <Toolbar className={classes.bar}>
+                    <IconButton className={classes.bgIcon} aria-label="cart" href={"/"}>
+                        <HomeIcon className={classes.icon}/>
+                    </IconButton>
+                    <IconButton className={classes.bgIcon} aria-label="cart" href={"/basket"}>
+                        <StyledBadge badgeContent={basket.length} color="secondary">
+                            <ShoppingBasketIcon className={classes.icon}/>
+                        </StyledBadge>
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
         </div>
     );
 }
